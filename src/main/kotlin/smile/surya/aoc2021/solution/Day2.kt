febb6aa97e0ac5f3bb0710517977113a66e32d19
suryamudti <suryamudti0128@gmail.com>
2021-12-31 07:00:39 +0800

    add day2 class

---
+++ src/main/kotlin/smile/surya/aoc2021/solution/Day2.kt
@@ -0,0 +1,17 @@
+package smile.surya.aoc2021.solution
+
+import smile.surya.aoc2021.template.Solution
+
+class Day2 : Solution() {
+    override fun solvePart1(input: String): Any {
+        TODO("Not yet implemented")
+    }
+
+    override fun solvePart2(input: String): Any {
+        TODO("Not yet implemented")
+    }
+}
+
+fun main() {
+
+}
