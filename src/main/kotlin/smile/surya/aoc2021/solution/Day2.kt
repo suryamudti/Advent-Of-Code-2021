1fd041684b39ba9479f7d747d46ba4130d6aa78b
suryamudti <suryamudti0128@gmail.com>
2021-12-31 08:57:19 +0800

    first try

--- src/main/kotlin/smile/surya/aoc2021/solution/Day2.kt
+++ src/main/kotlin/smile/surya/aoc2021/solution/Day2.kt
@@ -1,6 +1,7 @@
 package smile.surya.aoc2021.solution
 
 import smile.surya.aoc2021.template.Solution
+import smile.surya.aoc2021.util.splitMultiLine
 
 /**
  * Dive !
@@ -10,14 +11,29 @@ import smile.surya.aoc2021.template.Solution
 class Day2 : Solution() {
 
     override fun solvePart1(input: String): Any {
-        TODO("Not yet implemented")
+        val lines = input.splitMultiLine()
+        var horizontalPosition = 0
+        var depth = 0
+        lines.forEach {
+            val value = getValue(it)
+            if (it.startsWith("forward"))
+                horizontalPosition += value
+            else if (it.startsWith("up"))
+                depth -= value
+            else if (it.startsWith("down"))
+                depth += value
+        }
+        return horizontalPosition * depth
     }
 
     override fun solvePart2(input: String): Any {
-        TODO("Not yet implemented")
+        return ""
     }
+
+    private fun getValue(command: String) = command.split(" ")[1].toInt()
 }
 
 fun main() {
-
+    val day2 = Day2()
+    day2.solve()
 }
