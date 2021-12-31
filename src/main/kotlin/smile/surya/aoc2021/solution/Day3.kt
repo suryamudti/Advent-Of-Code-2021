1a584fbeb2bbfaf2f21e7b08ecf8652ae9590b91
suryamudti <suryamudti0128@gmail.com>
2021-12-31 10:54:26 +0800

    solve day3 part 1

--- src/main/kotlin/smile/surya/aoc2021/solution/Day3.kt
+++ src/main/kotlin/smile/surya/aoc2021/solution/Day3.kt
@@ -1,15 +1,48 @@
 package smile.surya.aoc2021.solution
 
 import smile.surya.aoc2021.template.Solution
+import smile.surya.aoc2021.util.binaryToDecimal
+import smile.surya.aoc2021.util.bitFlip
+import smile.surya.aoc2021.util.splitMultiLine
+
+/**
+ * Binary Diagnostic
+ * https://adventofcode.com/2021/day/3
+ * */
 
 class Day3 : Solution() {
 
     override fun solvePart1(input: String): Any {
-        TODO("Not yet implemented")
+        val rows: List<List<Int>> = input.toBitString()
+
+        val mostCommonBits: List<Int> = rows.first().indices.map { findMostCommonBit(rows, it) }
+        val leastCommonBits: List<Int> = mostCommonBits.map { it.bitFlip() }
+
+        val gammaRate: Int = mostCommonBits.binaryToDecimal()
+        val epsilonRate: Int = leastCommonBits.binaryToDecimal()
+
+        return gammaRate * epsilonRate
     }
 
     override fun solvePart2(input: String): Any {
-        TODO("Not yet implemented")
+        return  ""
     }
 
+
+    private fun String.toBitString() =
+        splitMultiLine()
+            .map { row -> row.split("")
+                .filter { it.isNotBlank() }
+                .map { it.toInt() }
+            }
+
+    private fun findMostCommonBit(rows: List<List<Int>>, column: Int, equallyCommonDefault: Int = 1): Int {
+        val meanBit: Double = rows.sumOf { it[column] } / rows.count().toDouble()
+        return if (meanBit == 0.5) equallyCommonDefault else if (meanBit > 0.5) 1 else 0
+    }
+}
+
+fun main() {
+    val day3 = Day3()
+    day3.solve()
 }
--- src/main/kotlin/smile/surya/aoc2021/util/Ext.kt
+++ src/main/kotlin/smile/surya/aoc2021/util/Ext.kt
@@ -1,3 +1,15 @@
 package smile.surya.aoc2021.util
 
 fun String.splitMultiLine() = split("\n")
+
+fun List<Int>.binaryToDecimal(): Int {
+    require(this.all { it == 0 || it == 1 }) { "Expected bit string, but received $this" }
+    return Integer.parseInt(this.joinToString(""), 2)
+}
+
+fun Int.bitFlip(): Int {
+    require(this == 0 || this == 1) {
+        "Expected bit, but received $this"
+    }
+    return this.xor(1)
+}
