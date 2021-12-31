82edaa4b8a389cb39725c1840747e72addc3f73e
suryamudti <suryamudti0128@gmail.com>
2021-12-31 08:50:04 +0800

    add links

--- src/main/kotlin/smile/surya/aoc2021/solution/Day2.kt
+++ src/main/kotlin/smile/surya/aoc2021/solution/Day2.kt
@@ -2,7 +2,13 @@ package smile.surya.aoc2021.solution
 
 import smile.surya.aoc2021.template.Solution
 
+/**
+ * Dive !
+ * https://adventofcode.com/2021/day/2
+ * */
+
 class Day2 : Solution() {
+
     override fun solvePart1(input: String): Any {
         TODO("Not yet implemented")
     }
