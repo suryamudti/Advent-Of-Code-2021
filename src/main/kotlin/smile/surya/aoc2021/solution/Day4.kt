d5d685e0f052a1895969d7f6e5601f8b7b146ae7
suryamudti <suryamudti0128@gmail.com>
2021-12-31 22:42:44 +0800

    init day4 class

---
+++ src/main/kotlin/smile/surya/aoc2021/solution/Day4.kt
@@ -0,0 +1,22 @@
+package smile.surya.aoc2021.solution
+
+import smile.surya.aoc2021.template.Solution
+
+class Day4 : Solution() {
+    override fun solvePart1(input: String): Any {
+
+        val lines = input.split("\n\n")
+
+        return lines
+    }
+
+    override fun solvePart2(input: String): Any {
+        TODO("Not yet implemented")
+    }
+}
+
+fun main() {
+   val day4 = Day4()
+   day4.solve()
+}
+
