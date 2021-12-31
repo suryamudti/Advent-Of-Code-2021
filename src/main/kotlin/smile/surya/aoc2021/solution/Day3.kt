d50a1b2875c6ffc2481f9e0aaffd0bfea6d9ab00
suryamudti <suryamudti0128@gmail.com>
2021-12-31 13:26:19 +0800

    solve part2

--- src/main/kotlin/smile/surya/aoc2021/solution/Day3.kt
+++ src/main/kotlin/smile/surya/aoc2021/solution/Day3.kt
@@ -25,12 +25,37 @@ class Day3 : Solution() {
     }
 
     override fun solvePart2(input: String): Any {
-        return ""
+        val rows: List<List<Int>> = input.toBitString()
+        val oxygenRating = calculateRating(rows, this::findMostCommonBit)
+        val co2ScrubberRating = calculateRating(rows, this::findLeastCommonBit)
+        return oxygenRating * co2ScrubberRating
     }
 
     private fun findMostCommonBit(rows: List<List<Int>>, column: Int, equallyCommonDefault: Int = 1): Int {
         val meanBit: Double = rows.sumOf { it[column] } / rows.count().toDouble()
         return if (meanBit == 0.5) equallyCommonDefault else if (meanBit > 0.5) 1 else 0
+    }
+
+    private fun findLeastCommonBit(rows: List<List<Int>>, column: Int, equallyCommonDefault: Int = 0): Int {
+        val meanBit: Double = rows.sumOf { it[column] } / rows.count().toDouble()
+        if (meanBit == 0.5) {
+            return equallyCommonDefault
+        }
+        val mostCommonBit: Int = if (meanBit > 0.5) 1 else 0
+        return mostCommonBit.bitFlip()
+    }
+
+    private fun calculateRating(rows: List<List<Int>>, bitFinder: (List<List<Int>>, Int) -> Int): Int {
+        var matches: List<List<Int>> = rows
+        for (column in rows.first().indices) {
+            val bit = bitFinder(matches, column)
+            matches = matches.filterNot { row -> row[column] != bit }
+            if (matches.count() == 1) break
+        }
+        require(matches.count() == 1) {
+            "Expected exactly one match to be left over"
+        }
+        return matches.first().binaryToDecimal()
     }
 }
 
