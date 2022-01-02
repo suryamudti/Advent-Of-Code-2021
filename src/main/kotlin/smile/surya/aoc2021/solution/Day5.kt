913a0e24a0317ccbd131a8a1c3625853159b6fca
suryamudti <suryamudti0128@gmail.com>
2022-01-02 16:39:13 +0800

    init day5 class

---
+++ src/main/kotlin/smile/surya/aoc2021/solution/Day5.kt
@@ -0,0 +1,22 @@
+package smile.surya.aoc2021.solution
+
+import smile.surya.aoc2021.template.Solution
+
+/**
+ * Hydrothermal Venture
+ * https://adventofcode.com/2021/day/5
+ * */
+
+class Day5 : Solution() {
+    override fun solvePart1(input: String): Any {
+        TODO("Not yet implemented")
+    }
+
+    override fun solvePart2(input: String): Any {
+        TODO("Not yet implemented")
+    }
+}
+
+fun main() {
+
+}
